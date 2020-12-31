423a086ccad33d06170df861f3d829ce892293e1
mwassen <marcuswassen@gmail.com>
2020-12-31 18:02:36 +0100

    fix font flicker

--- src/components/global-styles.tsx
+++ src/components/global-styles.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { Global, css, useTheme } from "@emotion/react";
-import manropeRegular2 from "../assets/fonts/Manrope-Regular.woff2";
-import manropeRegular from "../assets/fonts/Manrope-Regular.woff";
-import manropeBold2 from "../assets/fonts/Manrope-Bold.woff2";
-import manropeBold from "../assets/fonts/Manrope-Bold.woff";
+import "../assets/fonts/index.css";
 
 export const GlobalStyles: React.FC = () => {
     const theme = useTheme();
@@ -11,19 +8,6 @@ export const GlobalStyles: React.FC = () => {
     return (
         <Global
             styles={css`
-                @font-face {
-                    font-family: "Manrope";
-                    src: url(${manropeRegular2}) format("woff2"),
-                        url(${manropeRegular}) format("woff");
-                }
-
-                @font-face {
-                    font-family: "Manrope";
-                    font-weight: bold;
-                    src: url(${manropeBold2}) format("woff2"),
-                        url(${manropeBold}) format("woff");
-                }
-
                 html {
                     height: 100%;
                     background-color: ${theme.colors.accent};
