7ae52ddf5ede9062093b1187afb3d2ff4f54b3d5
mwassen <marcuswassen@gmail.com>
2021-01-05 17:55:11 +0100

    added icon to external link

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,13 +1,38 @@
 import React from "react";
+import LaunchSvg from "../assets/svg/launch.svg";
+import styled from "@emotion/styled";
+
+const ExternalA = styled.a`
+    white-space: nowrap;
+
+    &:hover {
+        opacity: 1;
+
+        svg {
+            opacity: 1;
+        }
+    }
+`;
+
+const LaunchIcon = styled(LaunchSvg)`
+    width: 0.9rem;
+    height: 0.9rem;
+    display: inline;
+    transform: translateY(0.1rem);
+    opacity: 0.5;
+    transition: opacity 200ms ease-in-out;
+    fill: var(--color-text);
+`;
 
 const ExternalLink: React.FC<{ href: string }> = ({ href, children }) => {
-    if (href.includes("mswsn.com") || href[0] === "/") {
+    if (href.includes("mswsn.com/") || href[0] === "/") {
         return <a href={href}>{children}</a>;
     }
     return (
-        <a href={href} target="_blank" rel="noopener noreferrer">
+        <ExternalA href={href} target="_blank" rel="noopener noreferrer">
             {children}
-        </a>
+            <LaunchIcon />
+        </ExternalA>
     );
 };
 
