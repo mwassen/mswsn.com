9fd5f7882c11108cda348477086377fe473392ee
mwassen <marcuswassen@gmail.com>
2021-01-08 18:45:19 +0100

    added splitter for hr

--- src/components/mdx/horizonal-line.tsx
+++ src/components/mdx/horizonal-line.tsx
@@ -1,7 +1,21 @@
 import styled from "@emotion/styled";
 
 export const HorizontalLine = styled.hr`
-    border: none;
-    border-top: 0.08rem solid var(--color-text);
     margin: 2rem 0;
+    border: none;
+    border-top: 1.5px solid var(--color-text);
+    color: var(--color-text);
+    overflow: visible;
+    text-align: center;
+    height: 5px;
+
+    &:after {
+        background: var(--color-background);
+        content: ">  <";
+        font-size: ${(props) => props.theme.type.scale[5]};
+        transition: background ${(props) => props.theme.animations.background};
+        padding: 0 4px;
+        position: relative;
+        top: -13px;
+    }
 `;
