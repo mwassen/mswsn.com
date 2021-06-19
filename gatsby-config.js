f82ff9322db471a19670db002ae526595cbb87db
mwassen <marcuswassen@gmail.com>
2021-06-19 12:30:41 +0200

    removed google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,6 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-emotion",
-        // {
-        //   resolve: "gatsby-plugin-google-analytics",
-        //   options: {
-        //     trackingId: "",
-        //   },
-        // },
         "gatsby-plugin-sharp",
 
         "gatsby-plugin-react-helmet",
